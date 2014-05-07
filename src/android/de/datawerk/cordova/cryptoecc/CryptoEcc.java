db325b1f851f839320d65b7ca04b7119e42fba02
Florin Peter <github@florin-peter.de>
2014-05-07 12:44:25 +0200

    added validateKey for android

--- src/android/de/datawerk/cordova/cryptoecc/CryptoEcc.java
+++ src/android/de/datawerk/cordova/cryptoecc/CryptoEcc.java
@@ -32,32 +32,12 @@ import com.neilalexander.jnacl.crypto.curve25519xsalsa20poly1305;
 
 public class CryptoEcc extends CordovaPlugin {
 	@Override
-	public boolean execute(final String action, JSONArray args,
-			final CallbackContext callbackContext) throws JSONException {
-		if ("deriveKey".equals(action)) {
-			final JSONObject params = parseParameters(args);
-			final String password = params.getString("password");
-			final byte[] salt = params.has("salt") ? NaCl.getBinary(params.getString("salt")) : null;
-
-			cordova.getThreadPool().execute(new Runnable() {
-				public void run() {
-					Pbkdf2 pbkdf2 = new Pbkdf2();
-					try {
-						byte[] encryptSalt = salt != null ? salt : new Random().randomBytes();
-						byte[] rawPassword = pbkdf2.encrypt(password, encryptSalt);
-
-						callbackContext.success(NaCl.asHex(rawPassword));
-					} catch (InvalidKeySpecException e) {
-						callbackContext.error(e.getMessage());
-					}
-				}
-			});
-		}
-
+	public boolean execute(final String action, JSONArray args, final CallbackContext callbackContext) throws JSONException {
+		
 		if ("getRandomValue".equals(action)) {
 			final JSONObject params = parseParameters(args);
 			final int len = params.has("length") ? params.getInt("length") : 16;
-
+            
 			cordova.getThreadPool().execute(new Runnable() {
 				public void run() {
 					final byte[] bytes = new Random().randomBytes(len);
@@ -65,17 +45,56 @@ public class CryptoEcc extends CordovaPlugin {
 				}
 			});
 		}
-
+		
+		if ("deriveKey".equals(action)) {
+			final JSONObject params = parseParameters(args);
+			final String password = params.getString("password");
+			final byte[] salt = params.has("salt") ? NaCl.getBinary(params.getString("salt")) : null;
+            
+			cordova.getThreadPool().execute(new Runnable() {
+				public void run() {
+					Pbkdf2 pbkdf2 = new Pbkdf2();
+					try {
+						byte[] encryptSalt = salt != null ? salt : new Random().randomBytes();
+						byte[] rawPassword = pbkdf2.encrypt(password, encryptSalt);
+                        
+						callbackContext.success(NaCl.asHex(rawPassword));
+					} catch (InvalidKeySpecException e) {
+						callbackContext.error(e.getMessage());
+					}
+				}
+			});
+		}
+        
+		if ("validateKey".equals(action)) {
+			final JSONObject params = parseParameters(args);
+			final String password = params.getString("password");
+			final String encryptedPassword = params.getString("encryptedPassword");
+			final byte[] salt = NaCl.getBinary(params.getString("salt"));
+            
+			cordova.getThreadPool().execute(new Runnable() {
+				public void run() {
+					Pbkdf2 pbkdf2 = new Pbkdf2();
+					try {
+						boolean valid = pbkdf2.validate(password, NaCl.getBinary(encryptedPassword), salt);
+						callbackContext.success(String.valueOf(valid));
+					} catch (InvalidKeySpecException e) {
+						callbackContext.error(e.getMessage());
+					}
+				}
+			});
+		}
+		
 		if ("generateKeyPair".equals(action)) {
 			cordova.getThreadPool().execute(new Runnable() {
 				public void run() {
-
+                    
 					byte[] pk = new byte[curve25519xsalsa20poly1305.crypto_secretbox_PUBLICKEYBYTES];
 					byte[] sk = new byte[curve25519xsalsa20poly1305.crypto_secretbox_SECRETKEYBYTES];
-
+                    
 					SecureRandom rng = new SecureRandom();
 					rng.nextBytes(sk);
-
+                    
 					curve25519xsalsa20poly1305.crypto_box_keypair(pk, sk);
 					final JSONObject object = new JSONObject();
 					try {
@@ -88,17 +107,17 @@ public class CryptoEcc extends CordovaPlugin {
 				}
 			});
 		}
-
+        
 		if ("decrypt".equals(action) || "encrypt".equals(action)) {
 			JSONObject params = parseParameters(args);
 			final String publicKey = (String) params.get("publicKey");
 			final String privateKey = (String) params.get("privateKey");
 			final byte[] nonce = NaCl.getBinary((String) params.get("nonce"));
 			final String data = (String) params.get("data");
-
+            
 			cordova.getThreadPool().execute(new Runnable() {
 				public void run() {
-
+                    
 					NaCl cryptoBox = null;
 					try {
 						cryptoBox = new NaCl(privateKey, publicKey);
@@ -107,22 +126,22 @@ public class CryptoEcc extends CordovaPlugin {
 						if ("encrypt".equals(action)) {
 							result = NaCl.asHex(cryptoBox.encrypt(data.getBytes(), nonce));
 						}
-
+                        
 						if ("decrypt".equals(action)) {
 							result = new String(cryptoBox.decrypt(NaCl.getBinary(data), nonce));
 						}
-
+                        
 						callbackContext.success(result);
 					} catch (Exception e) {
 						callbackContext.success("");
-					}					
+					}
 				}
 			});
 		}
-
+        
 		return true;
 	}
-
+    
 	private JSONObject parseParameters(JSONArray data) throws JSONException {
 		if (data.length() == 1 && !data.isNull(0)) {
 			return (JSONObject) data.get(0);
