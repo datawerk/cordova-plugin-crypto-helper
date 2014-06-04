ade18465f68cbf3ab66b94b9c3dced404ed236ac
Florin Peter <github@florin-peter.de>
2014-06-04 10:57:31 +0200

    Update CryptoEcc.m

--- src/ios/CryptoEcc.m
+++ src/ios/CryptoEcc.m
@@ -19,6 +19,7 @@
 #import "AGPBKDF2.h"
 #import "AGCryptoBox.h"
 #import "AGRandomGenerator.h"
+#import <CommonCrypto/CommonDigest.h>
 
 @implementation CryptoEcc
 
@@ -115,6 +116,19 @@
     }];
 }
 
+- (void)md5:(CDVInvokedUrlCommand *)command {
+    NSMutableDictionary *options = [self parseParameters:command];
+    NSString *data = [options objectForKey:@"data"];
+    
+    [self.commandDelegate runInBackground:^{
+        
+        NSString *md5String = [self MD5String:data];
+        
+        CDVPluginResult *pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:md5String];
+        [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+    }];
+}
+
 - (NSMutableData *)convertStringToData:(NSString *)hexString {
     NSMutableData *commandToSend = [[NSMutableData alloc] init];
     long byte;
@@ -127,6 +141,21 @@
         [commandToSend appendBytes:&byte length:1];
     }
     return commandToSend;
+}
+
+- (NSString *)MD5String:(NSString *)data {
+    const char *cstr = [data UTF8String];
+    
+    unsigned char result[16];
+    CC_MD5(cstr, strlen(cstr), result);
+    
+    return [NSString stringWithFormat:
+            @"%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X",
+            result[0], result[1], result[2], result[3],
+            result[4], result[5], result[6], result[7],
+            result[8], result[9], result[10], result[11],
+            result[12], result[13], result[14], result[15]
+            ];
 }
 
 - (NSString *)convertDataToString:(NSData *)data {
